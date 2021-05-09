b7a862f90164a64e4db8b42e8239880dfe272afb
le bao thinh <lebaothinh.krb@gmail.com>
2021-05-09 20:13:29 +0700

    Updates

--- static/1620550094/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fcart","\u002Fabout","\u002Fproduct","\u002Fcontact","\u002Fshop"]})
--- static/1620550094/shop/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002Fstatic\u002F1620550094",layout:"default",error:a,serverRendered:true,routePath:"\u002Fshop",config:{baseUrl:"localhost:3000\u002F",_app:{basePath:b,assetsPath:b,cdnURL:a}}}}(null,"\u002F"));
