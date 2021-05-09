92ea3fff282303c2fe8668c79b173f7c3185261f
le bao thinh <lebaothinh.krb@gmail.com>
2021-05-09 15:48:26 +0700

    Updates

--- _nuxt/static/1620549425/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1620549425",layout:"default",error:a,serverRendered:true,routePath:b,config:{baseUrl:"localhost:3000\u002F",_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,"\u002F"));
