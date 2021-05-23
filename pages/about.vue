0c69e1247c50aeedbf63a152c501f5c890e3c519
le bao thinh <lebaothinh.krb@gmail.com>
2021-05-23 08:43:47 +0700

    Add product 1

--- pages/about.vue
+++ pages/about.vue
@@ -1,10 +1,7 @@
 <template>
   <div>
       <!-- Title page -->
-	<section class="bg-img1 txt-center p-lr-15 p-tb-92" style="background-image: url('/images/bg-01.jpg');">
-		<h2 class="ltext-105 cl0 txt-center">
-			Giới Thiệu
-		</h2>
+	<section class="bg-img1 txt-center p-lr-15 p-tb-92" style="background-image: url('/images/background2.png');">
 	</section>	
 
 
@@ -42,7 +39,7 @@
 			</div> -->
 			
 			<div class="row">
-				<div class="order-md-2 col-md-7 col-lg-8 p-b-30">
+				<div class="order-md-2 col-md-12 col-lg-12 p-b-30">
 					<div class="p-t-7 p-l-85 p-l-15-lg p-l-0-md">
 						<h3 class="mtext-111 cl2 p-b-16">
 							Sứ Mệnh
@@ -54,23 +51,19 @@
 
 						<div class="bor16 p-l-29 p-b-9 m-t-22">
 							<p class="stext-114 cl6 p-r-40 p-b-11">
-								Creativity is just connecting things. When you ask creative people how they did something, they feel a little guilty because they didn't really do it, they just saw something. It seemed obvious to them after a while.
+								Targeting confident young ladies with nowadays modern lifestyles, N Soul offers a wide range of trendy outfits yet maintaining minimalist and elegant fashion styles.
 							</p>
-
-							<span class="stext-111 cl8">
-								- Steve Job’s 
-							</span>
 						</div>
 					</div>
 				</div>
 
-				<div class="order-md-1 col-11 col-md-5 col-lg-4 m-lr-auto p-b-30">
+				<!-- <div class="order-md-1 col-11 col-md-5 col-lg-4 m-lr-auto p-b-30">
 					<div class="how-bor2">
 						<div class="hov-img0">
 							<img src="images/about-02.jpg" alt="IMG">
 						</div>
 					</div>
-				</div>
+				</div> -->
 			</div>
 		</div>
 	</section>	
